e837e66467b0f9739b23e1b98d341eaaf7728dea
supremoods <80272399+supremoods@users.noreply.github.com>
2023-05-02 22:18:27 +0800

    sasda

--- mobile_dev/about_me/lib/constants/colors.dart
+++ mobile_dev/about_me/lib/constants/colors.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 const Color primaryColor = Color(0xFF9300FB);
 const Color secondaryColor = Color(0xFFFFC3A1);
 const Color darkColor = Color(0xFF303030);
+const Color lightColors = Color(0xFFE0E0E0);
 const Color lightColor = Color(0xFFE0E0E0);
 
 const Color black = Color(0xFF000000);
